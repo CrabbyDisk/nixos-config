1c06e1cc091b78b3fb1cdedcadda785a166354e3
Crabby Disk <crabbydisk@proton.me>
2024-12-13 21:13:03 -0500

    Install mpv

--- home/cli/default.nix
+++ home/cli/default.nix
@@ -12,9 +12,22 @@
       settings.flags.process_memory_as_value = true;
     };
     nushell.enable = true;
+    mpv = {
+      enable = true;
+      profiles = {
+        music = {
+          profile-cond = "(audio and not video)";
+          audio-display = "no";
+          audio-samplerate = 48000;
+        };
+      };
+    };
     gh = {
       enable = true;
-      settings.editor = "nvim";
+      settings = {
+        editor = "nvim";
+        git_protocol = "https";
+      };
     };
   };
 }
